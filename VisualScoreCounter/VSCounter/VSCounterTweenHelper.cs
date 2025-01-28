7057d91d9253a25faef87ebbe0cd2a56f2927135
BungiCraft <dagivans@gmail.com>
2025-01-28 06:48:44 +0100

    Updated for 1.39.1

--- VisualScoreCounter/VSCounter/VSCounterTweenHelper.cs
+++ VisualScoreCounter/VSCounter/VSCounterTweenHelper.cs
@@ -1,17 +1,10 @@
 ﻿using UnityEngine;
-using static BeatmapSaveDataVersion3.BeatmapSaveData;
 
 namespace VisualScoreCounter.VSCounter
 {
     internal class VSCounterTweenHelper : MonoBehaviour
     {
-        public VSCounterTweenHelper()
-        {
-            animationTime = 0.20f;
-            easeType = EaseType.InExpo;
-        }
-
-        public float animationTime { get; set; }
-        public EaseType easeType { get; set; }
+        public float animationTime { get; set; } = 0.20f;
+        public EaseType easeType { get; set; } = EaseType.InExpo;
     }
 }
